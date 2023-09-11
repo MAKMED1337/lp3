53d64dbba8186e0f463d603e079895cfa7ef05f6
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-09-11 01:01:50 +0000

    On `/reviews` return review balance for every account

--- bot/reviews.py
+++ bot/reviews.py
@@ -1,4 +1,4 @@
-import re
+import asyncio
 
 from telethon import events
 from telethon.types import Message
@@ -7,25 +7,19 @@ from log_scraper.logs import Logs
 from management.connected_accounts import ConnectedAccounts
 from management.users import Users
 
-from .config import BOT_NAME, bot
+from .config import bot
 
 
 @bot.on(events.NewMessage(pattern=r'^/reviews'))
 async def connect_account(msg: Message) -> None:
-    result = re.search(fr'^/reviews(?:@{BOT_NAME})? ([a-zA-Z0-9_.]+)$', msg.text)
-    if not result or len(result.groups()) != 1:
-        await msg.reply('Invalid wallet.\nUsage: /reviews test.near')
+    owner = await Users.get_by_tg(msg.sender_id)
+    accounts = [] if owner is None else await ConnectedAccounts.get_accounts(owner.id)
+    if not accounts:
+        await msg.reply('No accounts')
         return
 
-    account_id = result.group(1)
-    owner_id = await ConnectedAccounts.get_owner_id(account_id)
-    if owner_id is None:
-        await msg.reply('Unknown account')
-        return
-
-    user: Users = await Users.get(owner_id)  # type: ignore[assignment]
-    if user.tg_id != msg.sender_id:
-        await msg.reply('This account is not yours')
-        return
-
-    await msg.reply(str(await Logs.get_review_balance(account_id)))
+    text = ''
+    tasks = [Logs.get_review_balance(account_id) for account_id in accounts]
+    for account_id, balance in zip(accounts, await asyncio.gather(*tasks), strict=True):
+        text += f'{account_id} — {balance}'
+    await msg.reply(text)
