7b7b63602bc1e989553e9e9b5921cf2afda8b5da
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-09-11 21:16:49 +0000

    Add `/owner`

---
+++ bot/get_owner.py
@@ -0,0 +1,26 @@
+from telethon.types import Message
+
+from management.connected_accounts import ConnectedAccounts
+from management.users import Users
+
+from .config import bot
+from .helper import command_with_wallet, represent
+
+
+@command_with_wallet('owner')
+async def connect_account(msg: Message, account_id: str) -> None:
+    owner_id = await ConnectedAccounts.get_owner_id(account_id)
+    if owner_id is None:
+        await msg.reply('Unknown account')
+        return
+
+    print(owner_id)
+    owner = await Users.get(owner_id)
+    assert owner is not None
+
+    if owner.tg_id is None:
+        await msg.reply("User hasn't connected his tg")
+        return
+
+    user = await bot.get_entity(owner.tg_id)
+    await msg.reply(represent(user))
