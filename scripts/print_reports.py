640cd787fde11897a3f5a698db16673c4875d443
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-09-13 18:12:55 +0000

    On report command print previous reports

--- scripts/print_reports.py
+++ scripts/print_reports.py
@@ -5,14 +5,18 @@ from management.connected_accounts import ConnectedAccounts
 from management.reports import Reports
 
 
+async def print_reports(owner_id: int) -> None:
+    for num, report in enumerate(await Reports.get_reports(owner_id), 1):
+        print(f'Report #{num}:', report.text)
+
+
 async def main() -> None:
     await start_db()
     user_id = input('User id: ')
     owner_id = await ConnectedAccounts.get_owner_id(user_id)
     assert owner_id is not None
 
-    for report in await Reports.get_reports(owner_id):
-        print(f'Report #{report.id}:', report.text)
+    await print_reports(owner_id)
 
 
 if __name__ == '__main__':
--- scripts/report.py
+++ scripts/report.py
@@ -4,12 +4,17 @@ from helper.main_handler import main_handler
 from management.connected_accounts import ConnectedAccounts
 from management.reports import Reports
 
+from .print_reports import print_reports
+
 
 async def main() -> None:
     await start_db()
     user_id = input('User id: ')
     owner_id = await ConnectedAccounts.get_owner_id(user_id)
     assert owner_id is not None
+
+    print('Previous reports:')
+    await print_reports(owner_id)
 
     text = ''
     while (line := input('Text: ')) != '--end':
