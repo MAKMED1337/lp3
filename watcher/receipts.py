4364d70949e71a18741cb57a3a03ad3fa21f24c6
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-11-26 18:33:59 +0000

    Use paid reviews from watcher instead of `paid_reviews` from user

--- watcher/receipts.py
+++ watcher/receipts.py
@@ -24,7 +24,16 @@ class Receipts(Base):
 
     @staticmethod
     async def get_paid_reviews_per_user() -> dict[int, float]:
-        query = select(ConnectedAccounts.owner_id, func.sum(Receipts.reviews)) \
-            .join(Receipts, Receipts.account_id == ConnectedAccounts.user_id) \
-            .group_by(ConnectedAccounts.owner_id)
-        return dict(await db.fetch_all(query))  # type: ignore[arg-type]
+        return dict(await db.fetch_all(QUERY_PAID_REVIEWS))  # type: ignore[arg-type]
+
+    @staticmethod
+    async def get_paid_reviews_for_user(owner_id: int) -> float:
+        query = QUERY_PAID_REVIEWS.where(ConnectedAccounts.owner_id == owner_id)  # specify owner
+        result = await db.fetch_one(query)
+        return result[1] if result is not None else 0  # return only amount
+
+QUERY_PAID_REVIEWS = (
+    select(ConnectedAccounts.owner_id, func.sum(Receipts.reviews))
+    .join(Receipts, Receipts.account_id == ConnectedAccounts.user_id)
+    .group_by(ConnectedAccounts.owner_id)
+)
